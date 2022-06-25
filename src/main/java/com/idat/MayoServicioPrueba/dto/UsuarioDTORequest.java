6ce6ccdb8ada51300ecdfcc041c4bef2c8bf7fdc
Martin Mamani Napan <a20204009@idat.edu.pe>
2022-06-25 16:21:18 -0500

    Clase JWT

---
+++ src/main/java/com/idat/MayoServicioPrueba/dto/UsuarioDTORequest.java
@@ -0,0 +1,20 @@
+package com.idat.MayoServicioPrueba.dto;
+
+public class UsuarioDTORequest {
+	
+	private String usuario;
+	private String contrasenia;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contrasenia;
+	}
+	public void setContraseña(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+}
